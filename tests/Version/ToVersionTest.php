564624eccbdd08b2173a6823161018d6a5f96d1f
Lightman <yaruk.my@gmail.com>
2020-06-20 17:44:44 +0300

    add tests for Version::toVersion()

---
+++ tests/Version/ToVersionTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace Mrself\Version\Tests\Version;
+
+use Mrself\Version\Version;
+use PHPUnit\Framework\TestCase;
+
+class ToVersionTest extends TestCase
+{
+    public function testItCreatesNewVersionFromDottedByDotted()
+    {
+        $newVersion = Version::fromString('1.2.3')
+            ->toVersion('1.2.4');
+
+        $this->assertEquals('1.2.4', $newVersion);
+    }
+
+    /**
+     * @dataProvider getVersionMap
+     * @param string $namedVersion
+     * @param string $newVersion
+     * @throws \Mrself\Version\EInvalidArrayVersion
+     */
+    public function testItCreatesNewVersionFromDottedByNamedVersion(
+        string $namedVersion,
+        string $newVersion
+    ) {
+        $result = Version::fromString('1.2.3')
+            ->toVersion($namedVersion);
+
+        $this->assertEquals($newVersion, $result);
+    }
+
+    public function getVersionMap()
+    {
+        return [
+            ['major', '2.0.0'],
+            ['minor', '1.3.0'],
+            ['patch', '1.2.4']
+        ];
+    }
+}
